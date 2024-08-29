dd66ef8abf237ad76da35f3e81c9771ba08ac618
Chandan kumar Sah <93442672+sahchandan781@users.noreply.github.com>
2024-08-29 19:18:13 +0530

    implemented  video carousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        blue: '2997FF',
+        blue: '#2997FF',
         gray: {
           DEFAULT: "#86868b",
           100: "#94928d",
